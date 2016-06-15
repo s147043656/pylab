f23c6894bbfcd72c94d1e2355414e82d87900d66
master <master@m1604.vtdomain>
2016-06-15 11:39:34 +0300

    Add check it filenName already contains DataTaken

--- DataTaken/dtrename.py
+++ DataTaken/dtrename.py
@@ -15,7 +15,7 @@ filesDir = sys.argv[1]
 fileExt = sys.argv[2]
 filesList = os.listdir(filesDir)
 
-print('\n\tFull list of the ' + filesDir + ' directory:\n%s' % filesList)
+print('\n\t### Full list of the ' + filesDir + ' directory:\n%s' % filesList)
 print('\t(%d total)' % len(filesList))
 
 filesExtList = []
@@ -25,21 +25,27 @@ for item in filesList:
     if matchItem:
         filesExtList.append(item)
 
-print('\n\tFiltered to be processed (' + fileExt + ' only):\n%s' % filesExtList)
+print('\n\t### Filtered to be processed (' + fileExt + ' only):\n%s' % filesExtList)
 print('\t(%d total)' % len(filesExtList))
 
-print('\nOld file name:\t\tDate taken:\t\tNew file name:\n' + '-'*77)
+print('\n\t### Processing:\nOld file name\t\tDate taken\t\tNew file name\n' + '-'*77)
 if fileExt == 'JPG':
     os.chdir(filesDir)
+    processedFiles = 0
     for fileName in filesExtList:
         fileOpened = PIL.Image.open(fileName, 'r')
         dateTaken = fileOpened._getexif()[36867]
+        fileOpened.close()
         m1 = re.match('(.*):(.*):(.*)\ (.*):(.*):(.*)', dateTaken)
         dateTaken2 = m1.group(1) + m1.group(2) + m1.group(3) + '-' + m1.group(4) + m1.group(5) + m1.group(6)
-        newFileName = dateTaken2 + '-' + fileName 
-        print(fileName + '\t\t' + dateTaken + '\t' + newFileName)
-        fileOpened.close()
-        os.rename(fileName, newFileName)
+        m2 = re.match(dateTaken2, fileName)
+        if not m2:
+            newFileName = dateTaken2 + '-' + fileName 
+            print(fileName + '\t\t' + dateTaken + '\t' + newFileName)
+            fileOpened.close()
+            os.rename(fileName, newFileName)
+            processedFiles +=1 
+    print('\t(Total processed: %d' % processedFiles)
 
 if fileExt == 'MOV':
     print('Sorry, MOV renaming not developed yet.')
